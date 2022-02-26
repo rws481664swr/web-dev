9488b88110c9389a6a24ebf72ed1d5a83ca55d97
sam raphael <samraphael@gmail.com>
2022-02-26 18:58:59 -0500

    utility function(s)

---
+++ public/tuiter/common/index.js
@@ -0,0 +1,7 @@
+
+
+export const icons= (fa_classes, classes="", id="")=>` 
+<i id="${id}" class="${fa_classes} ${classes}"></i>
+`
+export const nbsp = "&nbsp;"
+export const checkedCircle= ()=> icons("fa-solid fa-circle-check")
