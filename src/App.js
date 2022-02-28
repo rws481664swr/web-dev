a563b0c69209faaafae3cab373cfed21a0ca6804
sam raphael <samraphael@gmail.com>
2022-02-28 18:15:49 -0500

    imports and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 // import './vendors/bootstrap/css/bootstrap.min.css';
 // import './vendors/bootstrap/bootstrap.min.css';
 // import './vendors/fontawesome/css/all.min.css';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
 import './App.css';
 import {BrowserRouter, Route } from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import Labs from "./components/Labs";
 import HelloWorld from "./components/HelloWorld";
 import Tuiter from "./components/Tuiter";
@@ -20,6 +24,9 @@ function App() {
                     <Route path={"/tuiter"} exact={true}>
                         <Tuiter/>
                     </Route>
+                <Route path={["/", '/labs']} exact={true} component={Labs}/>
+                <Route path={"/hello"} exact={true} component={HelloWorld}/>
+                <Route path={"/tuiter"} exact={true} component={Tuiter}/>
 
             </div>
         </BrowserRouter>
