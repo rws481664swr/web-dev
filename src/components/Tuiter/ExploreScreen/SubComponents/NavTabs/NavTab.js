beff84ae9283bb4165e3162505c097dd488f411c
sam raphael <samraphael@gmail.com>
2022-03-08 17:42:43 -0500

    cleaned up active logic

--- src/components/Tuiter/ExploreScreen/SubComponents/NavTabs/NavTab.js
+++ src/components/Tuiter/ExploreScreen/SubComponents/NavTabs/NavTab.js
@@ -1,8 +1,9 @@
 const NavTab = ({active, text}) => {
-    const isActive = text.toLowerCase() === (active.toLowerCase()) ? " active" : ''
     return (
         <li key={text} className={`nav nav-item`}>
-            <a href="#" className={`nav nav-link ${isActive} `}>
+            <a href="#" className={`nav nav-link ${
+                text.toLowerCase() === (active.toLowerCase())&&'active'
+            } `}>
                 {text}
             </a>
         </li>
