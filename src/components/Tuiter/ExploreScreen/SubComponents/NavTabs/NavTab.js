9f109925bd68e851553926727390879e8f96467f
sam raphael <samraphael@gmail.com>
2022-03-11 11:40:07 -0500

    ci fix

--- src/components/Tuiter/ExploreScreen/SubComponents/NavTabs/NavTab.js
+++ src/components/Tuiter/ExploreScreen/SubComponents/NavTabs/NavTab.js
@@ -1,9 +1,7 @@
 const NavTab = ({active, text,}) => {
-    const isactive = text.toLowerCase() === active
     return (
         <li key={text} className={`nav nav-item`}>
-            {/*<Anchor href={'#'} className={'nav nav-link'+('active')} content={text}/>*/}
-            <a href="#" className={`nav nav-link ${
+            <a href={"#"} className={`nav nav-link ${
                 text.toLowerCase() === (active.toLowerCase()) && 'active'
             } `}>
                 {text}
