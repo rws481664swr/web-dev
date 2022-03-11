edd7837a873ffba53b53a474974105fa3b6a95da
sam raphael <samraphael@gmail.com>
2022-03-11 11:37:20 -0500

    unused import

--- src/components/Tuiter.js
+++ src/components/Tuiter.js
@@ -1,17 +1,18 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import NavigationSidebar from "./Tuiter/NavigationSidebar";
 
-function Tuiter() { return(
-    <>
-        <h1>Tuiter</h1>
-        <Link to="/hello">
-            Hello
-        </Link> |
-        <Link to="/labs">
-            Labs
-        </Link>
+function Tuiter() {
+    return (
+        <>
+            <h1>Tuiter</h1>
+            <Link to="/hello">
+                Hello
+            </Link> |
+            <Link to="/labs">
+                Labs
+            </Link>
 
-    </>
-)};
+        </>
+    )
+};
 export default Tuiter;
