a5f4a16f745609e811d1b97615c7dda1adf33b8f
sam raphael <samraphael@gmail.com>
2022-03-13 09:27:23 -0400

    a6

--- src/components/Labs/Classes/index.js
+++ src/components/Labs/Classes/index.js
@@ -5,13 +5,6 @@ const Classes = () => {
     const dangerous = true;
     return( <div>
         <h2>Classes</h2>
-
-        {/*<div className="wd-bg-yellow wd-fg-black wd-padding-10px">*/}
-        {/*    Yellow background</div>*/}
-        {/*<div className="wd-bg-blue wd-fg-black wd-padding-10px">*/}
-        {/*    Blue background</div>*/}
-        {/*<div className="wd-bg-red wd-fg-black wd-padding-10px">*/}
-        {/*    Red background</div>*/}
         <div className={`wd-bg-${ color } wd-fg-black wd-padding-10px`}>
              Dynamic Blue background
         </div>
