2f4f1b1d4039a204d7fca41cd671c5535ab40752
sam raphael <samraphael@gmail.com>
2022-03-15 13:45:40 -0400

    who reducer stuff

--- src/components/Tuiter/WhoToFollowList/index.js
+++ src/components/Tuiter/WhoToFollowList/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import who from './who.json'
+
 import WhoToFollowListItem from "./WhoToFollowListItem.js";
+import {useSelector} from "react-redux";
 
 const WhoToFollowList = ()=> {
+        const who = useSelector((state) => state.who);
 return (
     <div className="list-group pt-3 wd-list-group-follow">
         <div className="list-group-item wd-list-group-item-follow fw-bold">
