c94ccc5e91a190b6d6f86e73ca781260148b23b5
sam raphael <samraphael@gmail.com>
2022-03-15 12:13:39 -0400

    Dummy NotificationScreen/index.js so Router doesn't complain

---
+++ src/components/Tuiter/NotificationScreen/index.js
@@ -0,0 +1,2 @@
+
+export default()=>{return <div></div>}
--- src/components/Tuiter/WhoToFollowList/index.js
+++ src/components/Tuiter/WhoToFollowList/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import who from './who.json'
 import WhoToFollowListItem from "./WhoToFollowListItem.js";
+import WhoToFollowListItem from "./WhoToFollowListItem";
 
 const WhoToFollowList = ()=> {
 return (
