537ec9b39a3759454188d00ed519b8a713d1cb1a
sam raphael <samraphael@gmail.com>
2022-03-15 12:09:00 -0400

    remove PostList in favor of TuitList (better name)

--- src/components/Tuiter/HomeScreen/HomeScreenComponent.js
+++ src/components/Tuiter/HomeScreen/HomeScreenComponent.js
@@ -3,10 +3,6 @@ import {Span} from "../common/index.js";
 
 const HomeScreenComponent = ()=>{
 return (
-    <div>
-        <Span content={"<a href='www.google.com'>google.com</a>"}/>
-        <PostList/>
-    </div>
 )
 }
 export default HomeScreenComponent
