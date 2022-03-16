73cbfa91466ab83f92031da7a24ea2583a20beb5
sam raphael <samraphael@gmail.com>
2022-03-15 21:51:28 -0400

    imported reducers, added to store

--- src/components/Tuiter/index.js
+++ src/components/Tuiter/index.js
@@ -1,34 +1,35 @@
 import {Outlet} from "react-router-dom";
 import NavigationSidebar from "./NavigationSidebar/index.js";
-import {navReducer, whoReducer} from "./reducers/index.js";
+import {whoReducer,navReducer, tuitReducer} from "../../reducers/index.js";
 import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
-import WhoToFollowList from "./WhoToFollowList";
+import WhoToFollowList from "./WhoToFollowList/index.js";
 
 export {default as UnderConstruction} from './UnderConstruction.js'
 export {default as HomeScreen} from './HomeScreen/HomeScreenComponent.js'
 export {default as ExploreScreen} from './ExploreScreen/ExploreComponent.js'
 export {default as NotificationScreen} from './NotificationScreen/index.js'
-
-const reducer = combineReducers({
-     who: whoReducer
-});
-const store = createStore(reducer);
+const reducers = combineReducers({
+    who:whoReducer,
+    nav:navReducer,
+    tuit:tuitReducer
+})
+const store = createStore(reducers)
 const Tuiter = () => {
     return (
         <Provider store={store}>
 
-        <div className="row mt-2">
-            <div className="col-2 col-lg-1 col-xl-2">
-                <NavigationSidebar/>
+            <div className="row mt-2">
+                <div className="col-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar/>
+                </div>
+                <div className="col-10 col-lg-7 col-xl-6">
+                    <Outlet/>
+                </div>
+                <div className="d-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList/>
+                </div>
             </div>
-            <div className="col-10 col-lg-7 col-xl-6">
-                <Outlet/>
-            </div>
-            <div className="d-none d-lg-block col-lg-4 col-xl-4">
-                <WhoToFollowList/>
-            </div>
-        </div>
         </Provider>
     );
 };
