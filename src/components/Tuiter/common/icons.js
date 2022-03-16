879f44a53ebbd1af730e639564675e23036c0905
sam raphael <samraphael@gmail.com>
2022-03-15 21:48:56 -0400

    added reusable icons .. more to follow

--- src/components/Tuiter/common/icons.js
+++ src/components/Tuiter/common/icons.js
@@ -1,3 +1,6 @@
 import React from "react";
 
 export const CircleIcon = () => <i className={'fa-solid fa-circle'}/>
+export const Heart =()=> <i className="far fa-heart me-1"/>
+export const HeartRed =()=> <i style={{color:'red'}} className="fas fa-heart me-1"/>
+export const RemoveIcon =({onClick,classNames})=><i onClick={onClick}  className={`${classNames} fas fa-remove fa-2x  fa-pull-right`}/>
