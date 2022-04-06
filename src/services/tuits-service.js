5152c0d87097ea818e1ce66fb4c6392a4963100c
sam raphael <samraphael@gmail.com>
2022-04-06 08:24:56 -0400

    setting up env variables

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 // const {get} = axios
-const BASE_URL = process.env.NUID ? 'http://localhost:4000' : 'https://sraph-web-dev-backend.herokuapp.com'
-
-const TUITS_API_REST = '/api/tuits'
-const TUITS_API = `${BASE_URL}${TUITS_API_REST}`
+// const BASE_URL = process.env.WEBDEV_PORT ? 'http://localhost:4000' : 'https://sraph-web-dev-backend.herokuapp.com'
+const API_BASE = process.env.REACT_APP_API_BASE;
+// console.log('API_BASE',API_BASE)
+const TUITS_API = `${API_BASE}/tuits`;
+console.log('TUITS_APi',TUITS_API)
+// console.log('env',process.env.REACT_APP_API_BASE)
+// console.log("api", process.env)
 
 export const findAllTuits = async () => {
     const response = await axios.get(TUITS_API);
