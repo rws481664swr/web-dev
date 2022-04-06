6b5cfd293c0dd3751db38d6fe3588c0eda5ce41e
sam raphael <samraphael@gmail.com>
2022-04-06 08:36:10 -0400

    api location . env stuff

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,12 +1,8 @@
 import axios from "axios";
-// const {get} = axios
-// const BASE_URL = process.env.WEBDEV_PORT ? 'http://localhost:4000' : 'https://sraph-web-dev-backend.herokuapp.com'
-const API_BASE = process.env.REACT_APP_API_BASE;
-// console.log('API_BASE',API_BASE)
+
+
+const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000';
 const TUITS_API = `${API_BASE}/tuits`;
-console.log('TUITS_APi',TUITS_API)
-// console.log('env',process.env.REACT_APP_API_BASE)
-// console.log("api", process.env)
 
 export const findAllTuits = async () => {
     const response = await axios.get(TUITS_API);
